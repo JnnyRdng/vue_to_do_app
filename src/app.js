44e50b79709d4cfddb35fa557f64bd1b7f586ea0
Jonny Reading <jonnyreading@gmail.com>
2020-08-24 15:42:33 +0100

    add form and method to push input to list

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     new Vue({
         el: "#app",
         data: {
-            todos: ["Learn Vue", "Write Vue", "Master Vue"]
+            todos: ["Learn Vue", "Write Vue", "Master Vue"],
+            newTodo: ""
+        },
+        methods: {
+            addTodo: function () {
+                this.todos.push(this.newTodo);
+                this.newTodo = "";
+            }
         }
     });
 });
